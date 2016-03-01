624e27b2b0107b13d56c0aadc0a8f442f14867ac
pateljigyasa <pateljigyasa@yahoo.co.in>
2016-03-01 12:26:05 +0530

    My First Commit,inline

---
+++ seastar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SeastarConfig(AppConfig):
+    name = 'seastar'
---
+++ seastar/urls.py
@@ -0,0 +1,12 @@
+""" URL configuraton for Sea Star website""" 
+from django.conf.urls import url
+from . import views 
+from django.conf.global_settings import STATICFILES_DIRS
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+ 
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),  
+   ] 
+ 
+
